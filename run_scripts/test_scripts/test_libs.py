375b0c4ad88d6815e49aa8a3cc62c51c7f84e357
AzharM <Gr33nMayhem@gmail.com>
2023-12-03 21:43:13 -0500

    [Compute-Canada] Fixed module import bugs

--- run_scripts/test_scripts/test_libs.py
+++ run_scripts/test_scripts/test_libs.py
@@ -1,7 +1,7 @@
+import sys
+sys.path.append("../../")
 from experiment import Exp
 import yaml
 import os
 import torch
 from ptflops import get_model_complexity_info
-import sys
-sys.path.append("../../")
