879b71e0e963b8b7836e3f244fff8eabeed74ffd
AzharM <Gr33nMayhem@gmail.com>
2023-12-04 21:39:47 -0500

    [Compute Canada] Time out in 1 hour, allocated more resources and set time out to 8 hour for test.

--- run_scripts/bash_jobs/test_job.sh
+++ run_scripts/bash_jobs/test_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=def-plago
 #SBATCH --export=ALL,DISABLE_DCGM=1
-#SBATCH --gpus-per-node=1
+#SBATCH --gpus-per-node=v100:4
 #SBATCH --mem=32G
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
-#SBATCH --time=12:0:0
+#SBATCH --ntasks-per-node=16
+#SBATCH --time=8:0:0
 #SBATCH --mail-user=Gr33nMayhem@gmail.com
 #SBATCH --mail-type=ALL
 
