0f4c6db0a452ae41d391738aa7cd5c087357f315
AzharM <Gr33nMayhem@gmail.com>
2023-12-13 19:38:23 -0500

    [Compute Canada] Changes to tarining scripts.

--- run_scripts/test_scripts/run_training.py
+++ run_scripts/test_scripts/run_training.py
@@ -1,8 +1,24 @@
 import sys
 import os
-
-sys.path.append(os.path.join("..", ".."))
+import argparse
 from run_scripts.test_scripts.train_with_data import run_train_process_with_data
 
-run_train_process_with_data('bluesense-RWR1')
-# run_train_process_with_data(sensors['bluesense-RWR2'])
+parser = argparse.ArgumentParser(
+    description='Enter a sensor name as --device <device name> \n ' +
+                'empatica-left: empatica\n' +
+                'empatica-right: empatica\n' +
+                'bluesense-LUA: bluesense\n' +
+                'bluesense-LWR: bluesense\n' +
+                'bluesense-RUA: bluesense\n' +
+                'bluesense-RWR1: bluesense\n' +
+                'bluesense-RWR2: bluesense\n' +
+                'bluesense-TRS: bluesense\n' +
+                'maxim-red: maxim\n' +
+                'maxim-green: maxim')
+parser.add_argument('--device', type=str, help='Device Name for training')
+
+args = parser.parse_args()
+
+sys.path.append(os.path.join("..", ".."))
+
+run_train_process_with_data(args.arg1)
