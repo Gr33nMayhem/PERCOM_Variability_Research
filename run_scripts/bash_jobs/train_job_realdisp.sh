d9ca1beb7ff2b1e6b863350b091037f643d9f7da
AzharM <Gr33nMayhem@gmail.com>
2024-02-19 09:47:27 -0500

    [REALDISP] Fixed typo.

--- run_scripts/bash_jobs/train_job_realdisp.sh
+++ run_scripts/bash_jobs/train_job_realdisp.sh
@@ -17,4 +17,4 @@ cd ~/projects/def-plago/akhaked/PERCOM_Variability_Model_Research/run_scripts/sc
 module purge
 module load python/3.11 scipy-stack
 source ~/py311/bin/activate
-python run_training_realdisp.py --device "$1" --varaint "$2"
+python run_training_realdisp.py --device "$1" --variant "$2"
