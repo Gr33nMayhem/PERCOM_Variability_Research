[realdisp] Fixed bug with normalization and added code of realdsip mmd creation.

run_scripts/scripts/run_mmd.py
@@ -8,22 +8,12 @@ import argparse
 sys.path.append(os.path.join("..", ".."))
 
 from dataloaders.utils import Normalizer
-from dataloaders import dataloader_HARVAR_har
+from dataloaders import dataloader_HARVAR_har, dataloader_REALDISP_har
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-parser = argparse.ArgumentParser(
-    description='Enter a sensor name as --device_train <device name> --device_test \n ' +
-                'empatica-left: empatica\n' +
-                'empatica-right: empatica\n' +
-                'bluesense-LUA: bluesense\n' +
-                'bluesense-LWR: bluesense\n' +
-                'bluesense-RUA: bluesense\n' +
-                'bluesense-RWR1: bluesense\n' +
-                'bluesense-RWR2: bluesense\n' +
-                'bluesense-TRS: bluesense\n' +
-                'maxim-red: maxim\n' +
-                'maxim-green: maxim')
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, help='Dataset Name')
 parser.add_argument('--device_train', type=str, help='Device Name of training')
 parser.add_argument('--device_test', type=str, help='Device Name of testing')
 
@@ -87,7 +77,60 @@ def MMD_with_sample(x, y, split_size, iterations, kernal):
     return mean_mmd, std_dev_mmd
 
 
-def load_all_the_data(device):
+def load_all_the_data_realdisp(reladisp_device):
+    root_path = os.path.join('..', '..', 'data', 'realdisp')
+    col_names = ['time_sec', 'time_msec', 'RLA_ACC_X', 'RLA_ACC_Y', 'RLA_ACC_Z', 'RLA_GYR_X', 'RLA_GYR_Y',
+                 'RLA_GYR_Z', 'RLA_MAG_X', 'RLA_MAG_Y', 'RLA_MAG_Z', 'RLA_ORI_1', 'RLA_ORI_2', 'RLA_ORI_3',
+                 'RLA_ORI_4', 'RUA_ACC_X', 'RUA_ACC_Y', 'RUA_ACC_Z', 'RUA_GYR_X', 'RUA_GYR_Y', 'RUA_GYR_Z',
+                 'RUA_MAG_X', 'RUA_MAG_Y', 'RUA_MAG_Z', 'RUA_ORI_1', 'RUA_ORI_2', 'RUA_ORI_3', 'RUA_ORI_4',
+                 'BACK_ACC_X', 'BACK_ACC_Y', 'BACK_ACC_Z', 'BACK_GYR_X', 'BACK_GYR_Y', 'BACK_GYR_Z',
+                 'BACK_MAG_X', 'BACK_MAG_Y', 'BACK_MAG_Z', 'BACK_ORI_1', 'BACK_ORI_2', 'BACK_ORI_3',
+                 'BACK_ORI_4', 'LUA_ACC_X', 'LUA_ACC_Y', 'LUA_ACC_Z', 'LUA_GYR_X', 'LUA_GYR_Y', 'LUA_GYR_Z',
+                 'LUA_MAG_X', 'LUA_MAG_Y', 'LUA_MAG_Z', 'LUA_ORI_1', 'LUA_ORI_2', 'LUA_ORI_3', 'LUA_ORI_4',
+                 'LLA_ACC_X', 'LLA_ACC_Y', 'LLA_ACC_Z', 'LLA_GYR_X', 'LLA_GYR_Y', 'LLA_GYR_Z', 'LLA_MAG_X',
+                 'LLA_MAG_Y', 'LLA_MAG_Z', 'LLA_ORI_1', 'LLA_ORI_2', 'LLA_ORI_3', 'LLA_ORI_4', 'RC_ACC_X',
+                 'RC_ACC_Y', 'RC_ACC_Z', 'RC_GYR_X', 'RC_GYR_Y', 'RC_GYR_Z', 'RC_MAG_X', 'RC_MAG_Y',
+                 'RC_MAG_Z', 'RC_ORI_1', 'RC_ORI_2', 'RC_ORI_3', 'RC_ORI_4', 'RT_ACC_X', 'RT_ACC_Y',
+                 'RT_ACC_Z', 'RT_GYR_X', 'RT_GYR_Y', 'RT_GYR_Z', 'RT_MAG_X', 'RT_MAG_Y', 'RT_MAG_Z',
+                 'RT_ORI_1', 'RT_ORI_2', 'RT_ORI_3', 'RT_ORI_4', 'LT_ACC_X', 'LT_ACC_Y', 'LT_ACC_Z',
+                 'LT_GYR_X', 'LT_GYR_Y', 'LT_GYR_Z', 'LT_MAG_X', 'LT_MAG_Y', 'LT_MAG_Z', 'LT_ORI_1',
+                 'LT_ORI_2', 'LT_ORI_3', 'LT_ORI_4', 'LC_ACC_X', 'LC_ACC_Y', 'LC_ACC_Z', 'LC_GYR_X',
+                 'LC_GYR_Y', 'LC_GYR_Z', 'LC_MAG_X', 'LC_MAG_Y', 'LC_MAG_Z', 'LC_ORI_1', 'LC_ORI_2',
+                 'LC_ORI_3', 'LC_ORI_4', 'activity_id', 'sub_id']
+    all_keys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+    print(" ----------------------- load all the data -------------------")
+    # split device string to get the variant and device
+    variant_to_use = reladisp_device.split("-")[1]
+    device_to_use = reladisp_device.split("-")[0]
+    all_data_df = pd.DataFrame()
+    for participant in all_keys:
+        # from data/realdisp load the .log file of type subject + participant + _ + variant.log
+        file_name = root_path + "/subject" + str(participant) + "_" + variant_to_use + ".log"
+        # load using pandas
+        data = pd.read_csv(file_name, names=col_names, sep='\t')
+        # add the subject column
+        data["sub_id"] = participant
+        # filter out columns from col_names where the column name starts with device + _ and only to use ACC data
+        columns_to_keep = [col for col in col_names if
+                           col.startswith(device_to_use + "_" + "ACC") or col in ["sub_id", "activity_id"]]
+        data = data[columns_to_keep].copy()
+        # remove all rows with activity 0
+        data = data[data["activity_id"] != 0]
+        # create a new column as a copy of sub_id as sub
+        data["sub"] = data["sub_id"].copy()
+        # make sub_id the first column in data
+        data = data[["sub_id"] + [col for col in data.columns if col != "sub_id"]]
+        # append the data to all_data_df
+        all_data_df = pd.concat([all_data_df, data])
+
+    # Y is the activity_id column
+    Y = all_data_df["activity_id"].reset_index(drop=True)
+    # X is all the columns except activity_id and sub_id
+    X = all_data_df.drop(columns=["activity_id"]).reset_index(drop=True)
+    return X, Y
+
+
+def load_all_the_data_harvar(device):
     print(" ----------------------- load all the data -------------------")
 
     user_dict = {
@@ -190,19 +233,36 @@ def normalization(train_vali, test=None):
         return train_vali_sensors, test_sensors
 
 
-def run(device1, device2):
+def run(dataset, device1, device2):
     if os.path.exists(os.path.join('..', '..', 'data', 'mmd', 'mmd_results_' + device1 + '_' + device2 + '.csv')):
         print('mmd results already exist')
         return
-    # sample compare between bluesense-RWR1 and bluesense-RWR2
-    # iterating through 8 cv
-    full_1_x, full_1_y = load_all_the_data(device1)
-    normalization(full_1_x)
-    full_2_x, full_2_y = load_all_the_data(device2)
-    normalization(full_2_x)
+
+    if dataset == 'harvar':
+        # harvar
+        # iterating through 8 cv
+        full_1_x, full_1_y = load_all_the_data_harvar(device1)
+        # normalization
+        full_1_x = normalization(full_1_x)
+        full_2_x, full_2_y = load_all_the_data_harvar(device2)
+        # normalization
+        full_2_x = normalization(full_2_x)
+        num_cv = 8
+
+    else:
+        # realdisp
+        # iterating through 34 cv
+        full_1_x, full_1_y = load_all_the_data_realdisp(device1)
+        # normalization
+        full_1_x = normalization(full_1_x)
+        full_2_x, full_2_y = load_all_the_data_realdisp(device2)
+        # normalization
+        full_2_x = normalization(full_2_x)
+        num_cv = 17
+
     # create a dataframe to store the mean mmd results on 3 axis
     mean_mmd = pd.DataFrame(columns=['CV', 'Acc_X', 'Acc_Y', 'Acc_Z', 'std_div_x', 'std_div_y', 'std_div_z'])
-    for i in range(1, 9):
+    for i in range(1, num_cv + 1):
         print('Starting cv', i)
         train = full_1_x[full_1_x['sub_id'] != i]
         test = full_2_x[full_2_x['sub_id'] == i]
@@ -226,7 +286,8 @@ def run(device1, device2):
 
 
 args = parser.parse_args()
-run(args.device_train, args.device_test)
-run(args.device_test, args.device_train)
-run(args.device_train, args.device_train)
-run(args.device_test, args.device_test)
+run(args.dataset, args.device_train, args.device_test)
+run(args.dataset, args.device_test, args.device_train)
+run(args.dataset, args.device_train, args.device_train)
+run(args.dataset, args.device_test, args.device_test)
+# run('realdisp', 'RLA-ideal', 'RLA-self')